effeaf2bd14b46f1c856fc08c4d4bc107e608767
emekarr <aceanyanwu02@gmail.com>
2021-11-23 15:42:13 +0100

    set up basic project structure

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,18 @@
-const nme = 'emeka';
-console.log(nme);
+import express, { Application } from 'express';
+
+class App {
+  private express: Application;
+
+  constructor() {
+    this.express = express();
+
+    this.express.use(express.json());
+    this.express.use(express.urlencoded({ extended: true }));
+  }
+
+  listen(port: string, cb: () => void) {
+    this.express.listen(port, cb);
+  }
+}
+
+export default new App();
